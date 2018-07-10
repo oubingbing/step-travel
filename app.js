a8c86bf8c5824177245813c8c5f1957dae3c094d
ouzhibing <ouzhibing@galaxymx.com>
2018-07-10 14:06:30 +0800

    初始化项目

---
+++ app.js
@@ -0,0 +1,176 @@
+
+App({
+  onLaunch: function () {
+
+    this.globalData.appKey = 'RF7r6G9tJa5qlkou';
+
+    //设置基本接口全局变量
+    this.globalData.apiUrl = 'https://lianyan.kucaroom.com/api/wechat';
+    //this.globalData.apiUrl = 'http://localhost:8000/api/wechat';
+  
+    //七牛图片外链域名
+    this.globalData.imageUrl = 'http://image.kucaroom.com/';
+    this.globalData.bgIimage = this.globalData.imageUrl+'30269a739a66831daa31ec93d28318af.jpg';
+
+    let token = wx.getStorageSync('token');
+    if (!token) {
+      let _this = this;
+      this.login();
+    } else {
+      console.log('token=' + token);
+    }
+  },
+
+  /**
+  * 登录获取token
+  */
+  login: function (_method = null, _url = null, _data = null, callback = null) {
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log(res);
+        this.getUserInfo(res.code, _method, _url, _data, callback);
+      }
+    })
+  },
+
+  /**
+   * 获取用户信息 
+   */
+  getUserInfo: function (code, _method = null, _url = null, _data = null, callback = null) {
+    console.log('get user info');
+    let that = this;
+    wx.getSetting({
+      success: res => {
+        console.log(res);
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              console.log("用户信息：" + JSON.stringify(res.userInfo));
+
+              wx.request({
+                url: this.globalData.apiUrl + '/auth/login?type=weChat',
+                header: {
+                  'content-type': 'application/json'
+                },
+                method: 'POST',
+                data: {
+                  user_info: res.userInfo,
+                  code: code,
+                  app_id: this.globalData.appKey
+                },
+                success: function (res) {
+                  wx.setStorageSync('token', res.data.data);
+                  console.log('token:' + res.data.data);
+                  if (_method) {
+                    that.http(_method, _url, _data, callback);
+                  }
+
+                  if(callback){
+                    callback();
+                  }
+                }
+              })
+            }
+          })
+        } else {
+          console.log('未授权');
+        }
+      }
+    })
+  },
+
+  /** 
+  * 封装微信http请求
+  */
+  http: function (_method, _url, _data, callback) {
+
+    console.log('method：' + _method);
+
+    let token = wx.getStorageSync('token');
+    let _this = this;
+
+    wx.request({
+      url: this.globalData.apiUrl + _url,
+      header: {
+        'content-type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      },
+      method: _method,
+      data: _data,
+      success: function (res) {
+
+        if (res.data.error_code == '4001' || res.data.error_code == '4000' || res.data.error_code == '5000') {
+          console.log('token过期了');
+          _this.login(_method, _url, _data, callback);
+        } else {
+          callback(res);
+        }
+
+      },
+      fail: function (res) {
+        console.log(res);
+        console.log('出错了');
+      }
+    })
+
+  },
+
+  /** 
+   * 获取七牛上传token
+   */
+  setUploadToken: function (call) {
+
+    this.http('GET', '/upload_token', {}, function (res) {
+
+      console.log('150行：'+res.data);
+
+      if(res.data.data != null){
+        var token = res.data.data.uptoken;
+
+        if (call) {
+          call(token);
+        }
+
+        console.log('设置七牛upload token' + token);
+
+        wx.setStorageSync('uploadToken', token);
+      }
+
+    });
+
+  },
+
+  /** 
+   * 获取七牛上传token
+   */
+  getUploadToken: function (callback) {
+
+    this.setUploadToken(callback);
+
+  },
+
+  /**
+   * 收集form id
+   */
+  collectFormId:function(formId){
+    this.http('POST', `/save_form_id`, {
+      form_id:formId
+    }, function (res) {
+      console.log(res);
+    });
+  },
+
+  globalData: {
+    appId:null,
+    userInfo: null,
+    apiUrl: null,
+    color: '0aecc3',
+    imageUrl:'',
+    bgImage:''
+  }
+})
---
+++ pages/compare_face/face.js
@@ -0,0 +1,299 @@
+const qiniuUploader = require("../../utils/qiniuUploader");
+const uploader = require("../../utils/uploadImage");
+
+const app = getApp()
+
+Page({
+  data: {
+    showSelect:false,
+    showBegin:true,
+    showCancel:false,
+    showReport: false,
+    bindReport:false,
+    showSubmit:false,
+    tryAgant:false,
+    imageLeft:'',
+    imageRight:'',
+    postImageLeft:'',
+    PostImageRight:'',
+    rate:0,
+    face:'',
+    conclusion:'',
+    show_auth: app.globalData.show_auth,
+    qrCode:'',
+    imageUrl: app.globalData.imageUrl
+  },
+
+  onLoad: function (option) {
+    this.hiddenSelect();
+    let that = this;
+    wx.getSetting({
+      success(res) {
+        if (!res.authSetting['scope.userInfo']) {
+          that.setData({
+            show_auth: true
+          });
+        }else{
+          //设置七牛上传token
+          app.getUploadToken(token => {
+            that.setData({
+              uploadToken: token
+            });
+          });
+          //获取二维码
+          that.getQrCode(that);
+        }
+      }
+    })
+  },
+
+  /**
+   * 监听用户点击授权按钮
+   */
+  getAuthUserInfo: function (data) {
+    app.globalData.show_auth = false;
+    this.setData({
+      show_auth: false
+    });
+
+    let _this = this;
+    app.login(null, null, null, function () {
+      console.log('登录中');
+      //设置七牛上传token
+      app.getUploadToken(token => {
+        _this.setData({
+          uploadToken: token
+        });
+      });
+      //获取二维码
+      _this.getQrCode(_this);
+    });
+  },
+
+  /**
+   * 显示
+   */
+  showSelect:function(){
+    this.setData({
+      showSelect: true,
+      showBegin: false,
+      showCancel: true
+    });
+  },
+
+  /**
+   * 隐藏
+   */
+  hiddenSelect:function(){
+    this.setData({
+      showSelect: false,
+      showReport: false,
+      bindReport: false
+    });
+  },
+
+  /**
+   * 取消选择
+   */
+  cancelSelect:function(){
+    this.setData({
+      showSelect: false,
+      showBegin: true,
+      showCancel: false,
+      bindReport: false
+    });
+  },
+
+  /**
+   * 选择左图
+   */
+  selectLeft:function(){
+
+    this.setData({ showReport: false })
+    let _this = this;
+    wx.chooseImage({
+      count: 1, 
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+
+        console.log('图片：' + res.tempFilePaths);
+        var tempFilePaths = res.tempFilePaths;
+        _this.setData({
+          imageLeft: tempFilePaths[0]
+        });
+        wx.showLoading({
+          title: '加载中',
+        });
+
+        uploader.upload(tempFilePaths[0], key => {
+          wx.hideLoading();
+          console.log(key);
+          _this.setData({
+            postImageLeft: app.globalData.imageUrl+key
+          });
+          if (_this.postImageLeft != '' && _this.PostImageRight != ''){
+            _this.setData({
+              showBegin: false,
+              showCancel: true,
+              showSubmit: true,
+              tryAgant: false
+            });
+          }
+        })
+
+      }
+    })
+  },
+
+  /**
+   * 选择图片
+   */
+  selectRight:function(){
+    this.setData({ showReport: false})
+    let _this = this;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+
+        console.log('图片：' + res.tempFilePaths);
+        var tempFilePaths = res.tempFilePaths;
+        _this.setData({
+          imageRight: tempFilePaths[0]
+        });
+        wx.showLoading({
+          title: '加载中',
+        });
+
+        uploader.upload(tempFilePaths[0], key => {
+          wx.hideLoading();
+          console.log(key);
+          _this.setData({
+            PostImageRight: app.globalData.imageUrl+key
+          });
+
+          if (_this.postImageLeft != '' && _this.PostImageRight != '') {
+            _this.setData({
+              showBegin: false,
+              showCancel: true,
+              showSubmit: true,
+              tryAgant: false,
+            });
+          }
+        })
+
+      }
+    })
+  },
+
+  /**
+   * 提交数据
+   */
+  submit:function(){
+    console.log(this.data.PostImageRight);
+    console.log(this.data.postImageLeft);
+
+    if (this.data.postImageLeft == ''){
+      wx.showLoading({
+        title: '左图上传失败，请重试',
+      });
+      setTimeout(function(){
+        wx.hideLoading();
+      },1500);
+      return false;
+    }
+
+    if (this.data.PostImageRight == '') {
+      wx.showLoading({
+        title: '右图上传失败，请重试',
+      });
+      setTimeout(function () {
+        wx.hideLoading();
+      }, 1500);
+      return false;
+    }
+
+    wx.showLoading({
+      title: '检测中',
+    });
+
+    app.http('post', `/compare_face`,
+      { your_face: this.data.postImageLeft, his_face: this.data.PostImageRight },         res => {
+        wx.hideLoading();
+        console.log('数据：' + JSON.stringify(res.data));
+        if (res.data.error_code){
+          wx.showLoading({
+            title: res.data.error_message,
+          });
+          setTimeout(function () {
+            wx.hideLoading();
+          }, 2000);
+          return false;
+        }
+      
+        let response = res.data;
+        this.setData({
+            rate: response.data.confidence,
+            face: response.data.key_world,
+            conclusion: response.data.message,
+            showReport: true,
+            bindReport: true
+        });
+      });
+  },
+
+  /** 
+   * 小程序的二维码
+   */
+  getQrCode: function (_this) {
+    app.http('GET', '/qr_code', {}, function (res) {
+      console.log(res.data.data.qr_code);
+      _this.setData({
+        qrCode: res.data.data.qr_code
+      })
+    });
+  },
+
+  /**
+   * 再来一次
+   */
+  tryAgant:function(){
+    this.setData({
+      rate: 0,
+      face: '',
+      conclusion: '',
+      showReport: false,
+      bindReport: false,
+      showCancel: true,
+      tryAgant: false,
+      showBegin: false,
+      showSubmit: false,
+      postImageLeft: '',
+      PostImageRight: '',
+      imageLeft: '',
+      imageRight: '',
+    });
+  },
+
+  /**
+   * 分享
+   */
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '天呐，我跟你竟然是天生的情侣脸',
+      path: 'pages/compare_face/face',
+      imageUrl: '/image/compare_face.jpg',
+      success: function (res) {
+      },
+      fail: function (res) {
+      }
+    }
+  },
+
+})
